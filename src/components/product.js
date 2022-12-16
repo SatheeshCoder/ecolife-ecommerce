3120baf982a3cf2def9c2dd9f8d44fafed51b7b7
satheeshtec <satheeshss2001@gmail.com>
2022-12-16 09:54:51 +0530

    complete product card

--- src/components/product.js
+++ src/components/product.js
@@ -1,11 +1,22 @@
-import React from "react";
-import { Col, Row, Container, Badge, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import {
+  Col,
+  Row,
+  Container,
+  Badge,
+  OverlayTrigger,
+  Tooltip,
+} from "react-bootstrap";
 import StarSharpIcon from "@mui/icons-material/StarSharp";
 import FavoriteBorderSharpIcon from "@mui/icons-material/FavoriteBorderSharp";
 import CompareArrowsIcon from "@mui/icons-material/CompareArrows";
+import SearchSharpIcon from "@mui/icons-material/SearchSharp";
 import ItemImg from "../img/item.jpg";
+import ItemImg1 from "../img/item2.jpg";
 import "../css/style.css";
 function Product() {
+  const [productList, setProuductList] = useState("");
+
   return (
     <Container>
       <Row>
@@ -13,6 +24,15 @@ function Product() {
           <section className="product_bg">
             <div className="item_image">
               <img src={ItemImg} alt="item_image" />
+              <Badge className="item_badge">new</Badge>
+            </div>
+            <div className="item_image1">
+              {/* <div className="searchcenter"> */}
+              <img src={ItemImg1} alt="item_image" />
+              <span className="circle">
+                <SearchSharpIcon />
+              </span>
+              {/* </div> */}
               <Badge className="item_badge">new</Badge>
             </div>
             <div className="item_text">
@@ -29,13 +49,39 @@ function Product() {
             <div className="overlay">
               {/* <Nav.Link href="#action1" className="overlay_ho"> */}
               <div className="overlay_ho">
-                <div className="overlay_ho1">add to cart</div>
+                <OverlayTrigger
+                  overlay={<Tooltip id="tooltip-disabled">add to cart</Tooltip>}
+                >
+                  <span className="d-inline-block">
+                    <div className="overlay_ho1">add to cart</div>
+                  </span>
+                </OverlayTrigger>
               </div>
 
               {/* </Nav.Link> */}
               <div className="overlay_ho">
-                <FavoriteBorderSharpIcon />
-                <CompareArrowsIcon />
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id="tooltip-disabled">add to wishlist</Tooltip>
+                  }
+                >
+                  <span className="d-inline-block">
+                    {" "}
+                    <FavoriteBorderSharpIcon />
+                  </span>
+                </OverlayTrigger>{" "}
+                <OverlayTrigger
+                  overlay={
+                    <Tooltip id="tooltip-disabled" className="tooltipbottom">
+                      add to compare
+                    </Tooltip>
+                  }
+                >
+                  <span className="d-inline-block">
+                    {" "}
+                    <CompareArrowsIcon />
+                  </span>
+                </OverlayTrigger>
               </div>
             </div>
           </section>
